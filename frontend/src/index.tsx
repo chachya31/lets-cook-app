412232fbdace68b845d4a66d6f1068dd7769494e
MyonsokuCha <myonsoku.cha@nsw.co.jp>
2026-01-27 18:03:37 +0900

    refactor(frontend): Redux完全削除
    - store.ts削除
    - slicesフォルダ削除
    - index.tsxからRedux Provider削除
    - Zustandへの完全移行完了

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,18 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import { store } from './store/store';
 import './i18n/i18n';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
